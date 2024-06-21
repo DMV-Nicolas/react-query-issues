19eb5518d3d4647ed522778579dd15e3adab73b5
DMV-Nicolas <dmvnicolas@gmail.com>
2024-06-21 17:39:57 -0500

    fetching labels

---
+++ src/issues/hooks/useLabels.ts
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi"
+import { sleep } from "../helpers/sleep"
+import { Label } from "../interfaces/label"
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2000)
+  const { data } = await githubApi.get<Label[]>("/labels")
+  return data
+}
+
+export function useLabels() {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 710400704,
+        node_id: "MDU6TGFiZWw3MTA0MDA3MDQ=",
+        url: "",
+        name: "test data",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 139734344,
+        node_id: "MDU6TGFiZWwxMzk3MzQzNDQ=",
+        url: "",
+        name: "test data",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 1757816973,
+        node_id: "MDU6TGFiZWwxNzU3ODE2OTcz",
+        url: "",
+        name: "test data",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 760751171,
+        node_id: "MDU6TGFiZWw3NjA3NTExNzE=",
+        url: "",
+        name: "test data",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 588833528,
+        node_id: "MDU6TGFiZWw1ODg4MzM1Mjg=",
+        url: "",
+        name: "test data",
+        color: "ffffff",
+        default: false,
+      },
+    ],
+  })
+
+  return { labelsQuery }
+}
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
 import { IssueList } from "../components/IssueList"
 import { LabelPicker } from "../components/LabelPicker"
 
 export const ListView = () => {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+
+  const handleChange = (labelName: string) => {
+    if (selectedLabels.includes(labelName)) {
+      setSelectedLabels((prev) => prev.filter((label) => label !== labelName))
+    } else {
+      setSelectedLabels((prev) => [...prev, labelName])
+    }
+  }
+
   return (
     <div className="row mt-5">
       <div className="col-8">
@@ -9,7 +20,7 @@ export const ListView = () => {
       </div>
 
       <div className="col-4">
-        <LabelPicker />
+        <LabelPicker selectedLabels={selectedLabels} onChange={handleChange} />
       </div>
     </div>
   )
